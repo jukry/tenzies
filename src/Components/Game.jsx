45173576beb30ddbcc078436f9c9be278ca62227
jukry <94563540+jukry@users.noreply.github.com>
2023-02-08 15:24:52 +0200

    game.jsx dice state, random array function and state updater button for testing

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 export default function Game() {
+    // set initial value as returned array from getArray
+    const [dice, setDice] = useState(() => getArray())
+    const [count, setCount] = useState(0)
+
+    function getArray() {
+        let arr = []
+        for (let i = 0; i < 10; i++) {
+            arr.push(Math.ceil(Math.random() * 6))
+        }
+        return arr
+    }
+    console.log(count)
+
     return (
-        <div>
+        <section>
+            {/* state tester */}
+            <button onClick={() => setCount(count + 1)}>Testeri {count}</button>
             <h1>Game</h1>
-        </div>
+        </section>
     )
 }
