096a152ec7c19b8d86d0613f6f111a94ea5de58f
jukry <94563540+jukry@users.noreply.github.com>
2023-02-08 15:01:39 +0200

    header content and styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header>
+            <h1>Tenzies</h1>
+            <p>
+                Roll until all dice are the same. Click each die to freeze it at
+                its current value.
+            </p>
+        </header>
     )
 }
