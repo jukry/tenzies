f4e87bcb5bfd91f6e1ca73515deef1b3fee2ad87
jukry <94563540+jukry@users.noreply.github.com>
2023-02-08 19:11:54 +0200

    refactored getArray function to return array with objects

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -6,18 +6,40 @@ export default function Game() {
     const [count, setCount] = useState(0)
 
     // function to return random array of 10 numbers between 1-6
+    /* function getArray() {
+        let arr = []
+        for (let i = 0; i < 10; i++) {
+            arr.push({
+                num: Math.ceil(Math.random() * 6),
+                id: i,
+            })
+        }
+        return arr
+    } */
+
     function getArray() {
         let arr = []
         for (let i = 0; i < 10; i++) {
-            arr.push(Math.ceil(Math.random() * 6))
+            arr.push({
+                num: Math.ceil(Math.random() * 6),
+                id: i,
+                frozen: false,
+            })
         }
         return arr
     }
 
+    /* console.log(dice) */
     // render dice to board with key, id and classname
-    const renderDice = dice.map((item, index) => (
+    /* const renderDice = dice.map((item, index) => (
         <div key={index} id={index} className={`key${index} dice`}>
             {item}
+        </div>
+    )) */
+
+    const renderDice = dice.map((item) => (
+        <div key={item.id} className={`key${item.id} dice`}>
+            {item.num}
         </div>
     ))
 
